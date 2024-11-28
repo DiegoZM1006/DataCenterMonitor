3414f6f39501252532261ddb5ade5d629f7064bd
Juan David Patiño Zambrano <juanda232405@hotmail.com>
2024-11-27 21:51:07 -0500

    Update app.sh

--- linux_monitor/app.sh
+++ linux_monitor/app.sh
@@ -3,9 +3,17 @@
 # Función para listar procesos en formato HTML que están en estado "Running"
 get_process_table() {
     echo "<table><tr><th>PID</th><th>Nombre</th><th>% Memoria</th><th>% CPU</th><th>Acción</th></tr>"
-    ps -eo pid,comm,state,%mem,%cpu --sort=-%mem | awk '$3 == "R" {printf "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td><button class=\"terminate-btn\" onclick=\"terminateProcess(%s)\">Terminar</button></td></tr>\n", $1, $2, $4, $5, $1}'
+    
+    # Usamos "comm" para obtener únicamente el nombre del ejecutable
+    ps -eo pid,comm,%mem,%cpu --no-headers --sort=-%mem | awk '{
+        mem=$3; cpu=$4; pid=$1; 
+        if (mem ~ /^[0-9]/ && cpu ~ /^[0-9]/) {
+            printf "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td><button class=\"terminate-btn\" onclick=\"terminateProcess(%s)\">Terminar</button></td></tr>\n", pid, $2, mem, cpu, pid
+        }
+    }'
     echo "</table>"
 }
+
 
 # Función para terminar un proceso por su ID
 terminate_process() {
